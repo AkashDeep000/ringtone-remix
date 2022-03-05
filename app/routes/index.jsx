c3497fe1f4439b864a2c233d05c633cb792cf76f
AkashDeep000 <akashdeepdas970@gmail.com>
2022-03-06 00:39:28 +0530

    1st

---
+++ app/routes/index.jsx
@@ -0,0 +1,11 @@
+import Header from "app/components/Header"
+import ItemCardGrid from "app/components/ItemCardGrid";
+import Head from "app/components/Head";
+export default function Index() {
+  return (
+    <>
+    <Header />
+    <Head/>
+    <ItemCardGrid /> < />
+  );
+}
